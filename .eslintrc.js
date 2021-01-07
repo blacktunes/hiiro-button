bbe17471b355e97b66dc5fcd69d1b1ae66f83fe6
blacktunes <blacktunes@foxmail.com>
2021-01-07 18:56:54 +0800

    build: 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
+    '@typescript-eslint/no-var-requires': 'off',
+    'dot-notation': 'off'
   }
 }
