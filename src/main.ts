931864104e096b60fa8a4eb71a908f79857f881d
blacktunes <blacktunes@foxmail.com>
2021-08-19 10:27:44 +0800

    refactor: 整理动画和过度样式

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import i18n from '@/assets/script/i18n'
 import router from '@/router'
 import '@/assets/script/analytics/index'
 import '@/assets/style/transition.styl'
+import '@/assets/style/animation.styl'
 
 createApp(App).use(router).use(i18n).mount('#app')
