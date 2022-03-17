1d0d9516eaab08a251d15482253510dce6c65436
blacktunes <blacktunes@foxmail.com>
2022-03-17 19:58:15 +0800

    fix: 修复依赖更新导致的打包错误

--- vue.config.js
+++ vue.config.js
@@ -19,10 +19,7 @@ module.exports = {
       }
     }
   },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = ['console.info']
-    }
+  configureWebpack: () => {
     return {
       plugins: [
         new Check(),
